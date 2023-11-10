e832f95c8ddcc17fb9857ae2cdca6752c4c27148
jacopicci <jacopicci@gmail.com>
2023-11-10 22:26:09 +0100

    Esercitazione 1

---
+++ Assets/CharMovement.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float maxSpeed;
+    [SerializeField] float jumpingHeight;
+    [SerializeField] float raycastDistance;
+    [SerializeField] float AirbornSpeedDivisor;
+
+    Vector3 vettoreMovimento = new Vector3(0, 0, 0);
+    Vector3 drag;
+    Rigidbody rb;
+    bool isMoving;
+    bool isAirborne;
+    bool jump;
+    
+    Vector3 jumpingForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpingForce = new Vector3(0,jumpingHeight,0);
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        CheckForGround();
+        isMoving = false;
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            vettoreMovimento.x -= speed * Time.deltaTime;
+            isMoving = true;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            vettoreMovimento.x += speed * Time.deltaTime;
+            isMoving = true;
+        }
+        if (Input .GetKeyDown(KeyCode.Space) && !isAirborne)
+        {
+            jump=true;
+        }
+
+
+    }
+    void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            
+            if (Mathf.Abs(rb.velocity.x) < maxSpeed)
+            {
+                if (!isAirborne)
+                {
+                    rb.velocity += vettoreMovimento;
+                }
+                else
+                {
+                    rb.velocity += vettoreMovimento / AirbornSpeedDivisor;
+                }
+            }        
+        }
+        else
+        {
+
+            drag = new Vector3(rb.velocity.x / 4f, 0, 0);
+            rb.velocity -= drag; 
+        }
+        Jump();
+
+        vettoreMovimento = Vector3.zero;
+    }
+    void Jump()
+    {
+        if (jump)
+        {
+            rb.AddForce(jumpingForce, ForceMode.Impulse);
+            jump = false;
+        }
+    }
+    void CheckForGround()
+    {
+        // Assuming you want to cast the ray from the object's position downward
+        Ray ray = new Ray(transform.position, Vector3.down);
+        isAirborne = true;
+        // Check if the ray hits something
+        if (Physics.Raycast(ray, out RaycastHit hit, raycastDistance))
+        {
+            // Check if the hit object has the specified tag
+            if (hit.collider.CompareTag("Ground"))
+            {
+                isAirborne = false;
+            }
+        }
+       
+            
+
+    }
+
+}
---
+++ Assets/CharacterState.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterState : MonoBehaviour
+{
+    bool isLightState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))              //<-Cambio di stato                                   
+        {                                                     //Dopo questo dovrai scrivere il tuo codice per cambiarlo                                   
+            changeState();                                    //Ti consiglio di creare due funzioni (come nell'esempio qui sotto), una per ogni stato                                  
+        }                                                     //
+    }                                                         //
+    void changeState()                                        //Concept dalla riunione:
+    {                                                         //Il protagonista dovrebbe diventare più lento, ma se prende velocità può distruggere alcuni oggetti
+        isLightState = !isLightState;                         //inoltre dovrà essere anche più leggero, in modo da poter rimbalzare 
+    }                                                         //aumentando l'altezza del salto cambiando forma in aria
+}
